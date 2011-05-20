16fab57bfb31869472b1702ed72c7c141c812f89
Nikita Zubkov <zubchick@yandex-team.ru>
2011-05-20 15:37:18 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = __import__('ndator').__version__
+print find_packages()
+setup(
+    name='django-ndator',
+    version=version,
+    author='Nikita Zubkov',
+    author_email='zubchick@gmail.com',
+    url='http://github.com/zubchick/django-ndator',
+    description='Django models obfuscator',
+    long_description=open('README.rst').read(),
+    license='New BSD License',
+    packages=find_packages(),
+    package_data = {'ndator': ['texts/*.txt']},
+    zip_safe=False,
+    include_package_data=True,
+
+    classifiers=[
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        ],
+)
